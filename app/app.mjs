0945f9c94d5bc465e0b46cd0bff495201e7db6e5
RY_2718(Ryo Yoshitani) <ry.3607x5531@gmail.com>
2018-10-17 18:54:39 +0900

    :sparkles: Reefactor: routerをディレクトリに切った

--- app/app.mjs
+++ app/app.mjs
@@ -1,11 +1,8 @@
 import express from 'express';
+import router from './route/v1';
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/api/v1', (req, res) => {
-  res.json({
-    message: 'hello world!',
-  });
-});
+app.use('/api/v1', router);
 
 app.listen(port);
