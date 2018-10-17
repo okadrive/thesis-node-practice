cdb6d9cdcdee7c2861563c16bebaffb4cf94615f
RY_2718(Ryo Yoshitani) <ry.3607x5531@gmail.com>
2018-10-17 19:53:11 +0900

    :art: Refactor: .eslintrc.jsをlintした

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,21 @@
 module.exports = {
-  "env": {
-    "es6": true,
-    "node": true
+  env: {
+    es6: true,
+    node: true,
   },
-  "extends": ["eslint:recommended", "prettier"],
-  "plugins": ["prettier"],
-  "parserOptions": {
-    "ecmaVersion": 2018,
-    "sourceType": "module"
+  extends: ['eslint:recommended', 'prettier'],
+  plugins: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
   },
-  "rules": {
-    "prettier/prettier": [
-      "error",
+  rules: {
+    'prettier/prettier': [
+      'error',
       {
-        "singleQuote": true,
-        "trailingComma": "es5",
-      }
+        singleQuote: true,
+        trailingComma: 'es5',
+      },
     ],
-  }
+  },
 };
