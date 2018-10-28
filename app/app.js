5d088fb238fdfb6c14c2e826e91f8fe4078a6dad
Ryo <zryooka0925@icloud.com>
2018-10-29 03:26:03 +0900

    make webpage

--- app/app.js
+++ app/app.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const mysql = require("mysql");
 const router = require('./route/v1');
-
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use('/api/v1', router);
+const connection = mysql.createPool({
+    host    : 'localhost',
+    user    : 'root',
+    password: 'root',
+    database: 'NodeTest'
+})
 
-app.listen(port);
+var sql = 'select * from test_table;';
+//webフォルダの中身を公開する
+app.use(express.static('web'));
+app.use('/api/v1', router);
+app.get('/api/v1/list', function(req, res){
+    connection.query(sql, function(error, results, fields){
+        if(error) throw error;
+        res.send(results);
+    })
+})
+
+app.listen(port, function(){
+    console.log('Listening on port '+ port +'...');
+});
